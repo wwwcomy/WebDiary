32b94b37d41520e0a746955c9e88c7e01bba1a4a
wwwcomy <wwwcomy@gmail.com>
2016-01-27 23:41:09 +0800

    Add JAXB annotation, but using jackson to generate the diary's json. Will try to understand the implementation of JAXB.

--- src/main/java/com/iteye/wwwcomy/model/Diary.java
+++ src/main/java/com/iteye/wwwcomy/model/Diary.java
@@ -10,89 +10,92 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
 
 @Entity
 @Table(name = "diary")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Diary {
 
-	private int id;
-	private User user;
-	private Date date;
-	/**
-	 * 上次更新日期
-	 */
-	private Date lastUpdateDate;
-	/**
-	 * 天气
-	 */
-	private String weather;
-	private String title;
-	private String content;
+    private int id;
+    private User user;
+    private Date date;
+    /**
+     * 上次更新日期
+     */
+    private Date lastUpdateDate;
+    /**
+     * 天气
+     */
+    private String weather;
+    private String title;
+    private String content;
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	public int getId() {
-		return id;
-	}
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public int getId() {
+        return id;
+    }
 
-	public void setId(int id) {
-		this.id = id;
-	}
+    public void setId(int id) {
+        this.id = id;
+    }
 
-	@ManyToOne
-	@JoinColumn(name = "userId")
-	// @Transient
-	public User getUser() {
-		return user;
-	}
+    @ManyToOne
+    @JoinColumn(name = "userId")
+    // @Transient
+    public User getUser() {
+        return user;
+    }
 
-	public void setUser(User user) {
-		this.user = user;
-	}
+    public void setUser(User user) {
+        this.user = user;
+    }
 
-	public Date getDate() {
-		return date;
-	}
+    public Date getDate() {
+        return date;
+    }
 
-	public void setDate(Date date) {
-		this.date = date;
-	}
+    public void setDate(Date date) {
+        this.date = date;
+    }
 
-	@Column(length = 16777215)
-	public String getContent() {
-		return content;
-	}
+    @Column(length = 16777215)
+    public String getContent() {
+        return content;
+    }
 
-	public void setContent(String content) {
-		this.content = content;
-	}
+    public void setContent(String content) {
+        this.content = content;
+    }
 
-	public Date getLastUpdateDate() {
-		return lastUpdateDate;
-	}
+    public Date getLastUpdateDate() {
+        return lastUpdateDate;
+    }
 
-	public void setLastUpdateDate(Date lastUpdateDate) {
-		this.lastUpdateDate = lastUpdateDate;
-	}
+    public void setLastUpdateDate(Date lastUpdateDate) {
+        this.lastUpdateDate = lastUpdateDate;
+    }
 
-	public String getWeather() {
-		return weather;
-	}
+    public String getWeather() {
+        return weather;
+    }
 
-	public void setWeather(String weather) {
-		this.weather = weather;
-	}
+    public void setWeather(String weather) {
+        this.weather = weather;
+    }
 
-	public String getTitle() {
-		return title;
-	}
+    public String getTitle() {
+        return title;
+    }
 
-	public void setTitle(String title) {
-		this.title = title;
-	}
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
-	@Override
-	public String toString() {
-		return "Diary content is:" + this.content;
-	}
+    @Override
+    public String toString() {
+        return "Diary content is:" + this.content;
+    }
 }
--- src/test/java/com/iteye/wwwcomy/model/TestDiary.java
+++ src/test/java/com/iteye/wwwcomy/model/TestDiary.java
@@ -1,5 +1,6 @@
 package com.iteye.wwwcomy.model;
 
+import java.io.IOException;
 import java.util.Date;
 
 import javax.annotation.Resource;
@@ -10,39 +11,62 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.fasterxml.jackson.core.JsonEncoding;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.iteye.wwwcomy.dao.DiaryDao;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration({ "classpath:applicationContext.xml" })
 public class TestDiary {
-	@Resource
-	private DiaryDao diaryDao;
+    @Resource
+    private DiaryDao diaryDao;
 
-	@Test
-	@Transactional
-	public void createDiary() {
-		Diary d = new Diary();
-		d.setContent("Content");
-		d.setDate(new Date());
-		d.setLastUpdateDate(new Date());
-		d.setUser(new User());
-		System.out.println(diaryDao.save(d));
-	}
+    @Test
+    @Transactional
+    public void createDiary() {
+        Diary d = new Diary();
+        d.setContent("Content");
+        d.setDate(new Date());
+        d.setLastUpdateDate(new Date());
+        d.setUser(new User());
+        System.out.println(diaryDao.save(d));
+    }
 
-	@Test
-	@Transactional
-	public void loadDiary() {
-		User user = new User();
-		user.setUserid(1);
-		Diary d = diaryDao.getByDate(user, new Date());
-		System.out.println(d);
-	}
+    @Test
+    @Transactional
+    public void loadDiary() {
+        User user = new User();
+        user.setUserid(1);
+        Diary d = diaryDao.getByDate(user, new Date());
+        System.out.println(d);
+    }
 
-	public DiaryDao getDiaryDao() {
-		return diaryDao;
-	}
+    @Test
+    public void toJson() throws Exception {
+        Diary d = new Diary();
+        d.setContent("Content");
+        d.setDate(new Date());
+        d.setLastUpdateDate(new Date());
+        d.setUser(new User());
+        d.setId(123);
+        d.setTitle("title");
+        d.setWeather("weather");
+        JsonGenerator jsonGenerator = null;
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            jsonGenerator = objectMapper.getFactory().createGenerator(System.out, JsonEncoding.UTF8);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        jsonGenerator.writeObject(d);
+    }
 
-	public void setDiaryDao(DiaryDao diaryDao) {
-		this.diaryDao = diaryDao;
-	}
+    public DiaryDao getDiaryDao() {
+        return diaryDao;
+    }
+
+    public void setDiaryDao(DiaryDao diaryDao) {
+        this.diaryDao = diaryDao;
+    }
 }
