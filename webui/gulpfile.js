489e69b0bf8a294093bfa2b686b0c8d9630ac584
wwwcomy <wwwcomy@gmail.com>
2016-12-19 17:01:09 +0800

    refine the gulpfile.js to make it work

--- webui/gulpfile.js
+++ webui/gulpfile.js
@@ -1,100 +1,81 @@
 var gulp = require('gulp');
-//引入gulp组件
-//js语法检查
-var jshint = require('gulp-jshint');
-//sass预处理
-var sass = require('gulp-sass');
-//文件合并
+var clean = require('gulp-clean');
 var concat = require('gulp-concat');
-//js压缩
-var uglify = require('gulp-uglify');
-//重命名
-var rename = require('gulp-rename');
-//server服务
-browserSync = require('browser-sync').create();
+var browserSync = require('browser-sync').create();
+var runSequence = require('run-sequence');
 
-var jsFiles = [
-  './node_modules/angular/angular.js',
-  './node_modules/angular-ui-router/release/angular-ui-router.js'
-];
+var paths = {
+	js : [ 'src/js/index.js' ],
+	css : [ 'src/css/*' ],
+	img : [ 'src/img/*' ],
+	html : [ 'src/html/*' ],
+	lib : {
+		js : [ 'node_modules/jquery/dist/jquery.js',
+				'node_modules/bootstrap/dist/js/bootstrap.js' ],
+		css : [ 'node_modules/bootstrap/dist/css/bootstrap.css' ],
+		img : []
+	}
+};
 
-//检查脚本
-gulp.task('lint', function() {
-  gulp.src('./app/**/*.js')
-          .pipe(jshint())
-          .pipe(jshint.reporter('default'));
+gulp.task('clean', function() {
+	return gulp.src('dist', {
+		read : false
+	}).pipe(clean());
+});
 
-})
-
-//编译Sass
-gulp.task('sass', function() {
-  console.log('sass!');
-  gulp.src('./app/style/*.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('./app/style'));
-  gulp.src('./app/style/*.css')
-    .pipe(concat('style.css'))
-    .pipe(gulp.dest('./dist/css'))
-})
-
-//合并、压缩js文件
-gulp.task('scripts', function() {
-  gulp.src('./app/**/*.js')
-    .pipe(concat('all.js'))
-    .pipe(gulp.dest('./dist/js'))
-    .pipe(rename('all.min.js'))
-    .pipe(uglify())
+gulp.task('build-dep-js', function() {
+  return gulp.src(paths.lib.js)
+    .pipe(concat('dependent.js'))
     .pipe(gulp.dest('./dist/js'));
 });
-//合并、压缩来自npm的js资源文件
-gulp.task('npmscripts', function() {
-  return gulp.src(jsFiles)
-  .pipe(concat('npm.js'))
-  .pipe(gulp.dest('./dist/js'))
-  .pipe(rename('npm.min.js'))
-  .pipe(uglify())
-  .pipe(gulp.dest('./dist/js'));
-})
 
-//合并、压缩css文件
-// gulp.task('css', function() {
-//   gulp.src('./app/style/*.css')
-//     .pipe(concat('style.css'))
-//     .pipe(gulp.dest('./dist/css'))
-//     // .pipe(rename('style.min.css'))
-//     // .pipe(uglify())
-//     // .pipe(gulp.dest('./dist/css'));
-// });
+gulp.task('build-dep-css', function() {
+  return gulp.src(paths.lib.css)
+    .pipe(concat('dependent.css'))
+    .pipe(gulp.dest('./dist/css'));
+});
 
-//使用connect启动一个Web服务器
+gulp.task('build-html', function() {
+  gulp.src(paths.html).pipe(gulp.dest('./dist'));
+});
+
+gulp.task('build-img', function() {
+  gulp.src(paths.img).pipe(gulp.dest('./dist/img'));
+});
+
+gulp.task('build-js', function() {
+	gulp.src(paths.js).pipe(concat('main.js')).pipe(gulp.dest('./dist/js'));
+	browserSync.reload();
+});
+
+gulp.task('build-js-watch', [ 'build-js' ], function(done) {
+	browserSync.reload();
+	done();
+});
+
+gulp.task('build-html-watch', [ 'build-html' ], function() {
+	browserSync.reload();
+	done();
+});
+
 gulp.task('browserSync', function () {
   browserSync.init({
          server: {
-             baseDir: "./app/"
+             baseDir: "./dist/"
          }
      });
 });
 
-//默认任务
-gulp.task('default', function() {
-  gulp.run('npmscripts');
+gulp.task('watch', function() {
+  gulp.watch('src/js/**/*.js', ['build-js-watch']);
+  gulp.watch(paths.html, ['build-html-watch']);
+});
 
-//监听js变化
-browserSync.init({
-       server: {
-           baseDir: "./"
-       }
-   });
+gulp.task('build', ['build-dep-js', 'build-dep-css', 'build-html', 'build-js', 'build-img'], function() {});
 
-gulp.watch('./app/**/*.js', ['lint', 'scripts']);
+gulp.task('dev', [], function() {
+	runSequence('clean', [ 'build', 'browserSync', 'watch' ]);
+});
 
-gulp.watch('./app/style/**/*.scss', ['sass']);
-
-// gulp.watch('./app/style/**/*.css', ['css'])
-
-gulp.watch('./app/**', function() {
-       console.log('reload');
-       browserSync.reload();
-   });
-
-})
+gulp.task('default',['clean'], function() {
+});
