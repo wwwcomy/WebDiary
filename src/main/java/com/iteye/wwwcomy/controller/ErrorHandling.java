8027abce3c792e69a1750d1d855c35b1a8a379a5
wwwcomy <wwwcomy@gmail.com>
2018-06-23 21:33:07 +0800

    exception handler

--- src/main/java/com/iteye/wwwcomy/controller/ErrorHandling.java
+++ src/main/java/com/iteye/wwwcomy/controller/ErrorHandling.java
@@ -2,33 +2,86 @@ package com.iteye.wwwcomy.controller;
 
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.iteye.wwwcomy.model.dto.ValidationErrorMessage;
-import com.iteye.wwwcomy.model.exception.NotFoundException;
+import com.iteye.wwwcomy.model.exception.EntityAlreadyExistsException;
+import com.iteye.wwwcomy.model.exception.EntityNotFoundException;
+import com.iteye.wwwcomy.model.exception.InvalidParameterException;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class ErrorHandling {
-	@ExceptionHandler(NullPointerException.class)
-	@ResponseBody
+
+	private final Logger logger = LoggerFactory.getLogger(getClass());
+
+	@ExceptionHandler(Exception.class)
 	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-	public ValidationErrorMessage handle(NullPointerException ex, HttpServletResponse response) {
+	public ValidationErrorMessage handle(Exception ex, HttpServletResponse response) {
+		logger.error(ex.getMessage(), ex);
 		ValidationErrorMessage message = new ValidationErrorMessage(HttpStatus.INTERNAL_SERVER_ERROR.toString(),
 				ex.getMessage());
 		response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
 		return message;
 	}
 
-	@ExceptionHandler(NotFoundException.class)
-	@ResponseBody
+	@ExceptionHandler(EntityNotFoundException.class)
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	public ValidationErrorMessage handleNotFoundException(NullPointerException ex, HttpServletResponse response) {
+		logger.error(ex.getMessage(), ex);
 		ValidationErrorMessage message = new ValidationErrorMessage(HttpStatus.BAD_REQUEST.toString(), ex.getMessage());
 		response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
 		return message;
 	}
+
+	@ExceptionHandler(Exception.class)
+	@ResponseStatus(HttpStatus.SERVICE_UNAVAILABLE)
+	public ValidationErrorMessage handle(Exception ex) {
+		logger.error(ex.getMessage(), ex);
+		return new ValidationErrorMessage("exception", "An error occurred:" + ex.getMessage());
+	}
+
+	/**
+	 * Catches any {@link EntityAlreadyExistsException} and returns a response with
+	 * bad request status.
+	 *
+	 */
+	@ExceptionHandler({ EntityAlreadyExistsException.class })
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	public ValidationErrorMessage handle(EntityAlreadyExistsException ex, HttpServletResponse response) {
+		logger.error(ex.getMessage(), ex);
+		response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+		return new ValidationErrorMessage("exception", ex.getMessage());
+	}
+
+	/**
+	 * Catches any {@link EntityNotFoundException} and returns a response with not
+	 * found status.
+	 *
+	 */
+	@ExceptionHandler({ EntityNotFoundException.class })
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	public ValidationErrorMessage handle(EntityNotFoundException ex, HttpServletResponse response) {
+		logger.error(ex.getMessage(), ex);
+		response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+		return new ValidationErrorMessage("exception", ex.getMessage());
+	}
+
+	/**
+	 * Catches any {@link EntityNotFoundException} and returns a response with 401
+	 * status.
+	 *
+	 */
+	@ExceptionHandler({ InvalidParameterException.class })
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	public ValidationErrorMessage handle(InvalidParameterException ex, HttpServletResponse response) {
+		logger.error(ex.getMessage(), ex);
+		response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+		return new ValidationErrorMessage("exception", ex.getMessage());
+	}
+
 }
