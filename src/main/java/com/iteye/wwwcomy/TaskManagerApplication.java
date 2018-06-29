23e944c7b1046e876ceb3bfd59a9d7254ac4f1a2
wwwcomy <wwwcomy@gmail.com>
2018-06-29 21:56:12 +0800

    Move swagger ui configuration out of the main configuration

--- src/main/java/com/iteye/wwwcomy/TaskManagerApplication.java
+++ src/main/java/com/iteye/wwwcomy/TaskManagerApplication.java
@@ -1,11 +1,7 @@
 package com.iteye.wwwcomy;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -13,16 +9,6 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 
 import com.iteye.wwwcomy.main.Migrater;
 
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.ParameterBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.schema.ModelRef;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.service.Parameter;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -36,22 +22,4 @@ public class TaskManagerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TaskManagerApplication.class, args);
 	}
-
-	@Bean
-	public Docket createRestApi() {
-		ParameterBuilder tokenPar = new ParameterBuilder();
-		List<Parameter> pars = new ArrayList<Parameter>();
-		tokenPar.name("token").description("Token").modelRef(new ModelRef("string")).parameterType("header")
-				.required(false).build();
-		pars.add(tokenPar.build());
-		return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select()
-				.apis(RequestHandlerSelectors.basePackage("com.iteye.wwwcomy.controller")).paths(PathSelectors.any())
-				.build().globalOperationParameters(pars);
-	}
-
-	private ApiInfo apiInfo() {
-		Contact contact = new Contact("wwwcomy", "", "wwwcomy@gmail.com");
-		return new ApiInfoBuilder().title("Web Diary").description("Web Diary").contact(contact).version("1.0").build();
-	}
-
 }
--- src/main/java/com/iteye/wwwcomy/config/ExtraConfig.java
+++ src/main/java/com/iteye/wwwcomy/config/ExtraConfig.java
@@ -18,4 +18,21 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
-public class ExtraConfig {}
+public class ExtraConfig {
+	@Bean
+	public Docket createRestApi() {
+		ParameterBuilder tokenPar = new ParameterBuilder();
+		List<Parameter> pars = new ArrayList<Parameter>();
+		tokenPar.name("token").description("Token").modelRef(new ModelRef("string")).parameterType("header")
+				.required(false).build();
+		pars.add(tokenPar.build());
+		return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select()
+				.apis(RequestHandlerSelectors.basePackage("com.iteye.wwwcomy.controller")).paths(PathSelectors.any())
+				.build().globalOperationParameters(pars);
+	}
+
+	private ApiInfo apiInfo() {
+		Contact contact = new Contact("wwwcomy", "", "wwwcomy@gmail.com");
+		return new ApiInfoBuilder().title("Web Diary").description("Web Diary").contact(contact).version("1.0").build();
+	}
+}
